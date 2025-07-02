7ab7c69cf6a68854942204a3699564ec24aa4ceb
suralokin123 <kolya_romashkin@bk.ru>
2025-07-02 08:46:51 +0300

    обновил разделы

--- js/burger-start.js
+++ js/burger-start.js
@@ -1,33 +1,11 @@
 const burgerMenu = document.querySelector('.burgers-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobiles-menu');
 const body = document.body;
-let scrollPosition = 0;
-const mobileLinks = document.querySelectorAll('.mobile-menu a');
 
 burgerMenu.addEventListener('click', () => {
-  const isMenuOpen = mobileMenu.style.display === 'flex';
+    burgerMenu.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
 
-  if (isMenuOpen) {
-    mobileMenu.style.display = 'none';
-    body.style.position = 'static';
-    body.style.top = 'auto';
-    window.scrollTo(0, scrollPosition);
-    burgerMenu.classList.remove('active'); // Удаляем класс
-  } else {
-    scrollPosition = window.pageYOffset;
-    mobileMenu.style.display = 'flex';
-    body.style.position = 'fixed';
-    body.style.top = `-${scrollPosition}px`;
-    burgerMenu.classList.add('active'); // Добавляем класс
-  }
-  mobileLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      mobileMenu.style.display = 'none';
-      body.style.position = 'static';
-      body.style.top = 'auto';
-      window.scrollTo(0, scrollPosition);
-      burgerMenu.classList.remove('active');
-    });
-  });
-  
+
+    body.classList.toggle('no-scroll');
 });
